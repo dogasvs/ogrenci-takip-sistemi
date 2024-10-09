506fe859993d3feecb253cd2892ca15323d2cbde
Doğa Savaş <doasavaas@gmail.com>
2024-10-09 20:03:51 +0300

    login sayfasının tasarımı yapıldı svgs klasörü oluşturuldu public klasöörü oluşturuldu

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,14 +6,15 @@ export default function LoginPage() {
   return (
    <form className="loginForm">
       <div className="logo">
-      <Logo /> 
-      <h1>Login</h1>
+        <Logo /> 
+        <h1>Login</h1>
       </div>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <label htmlFor="email">Email</label>
+      <input id="email" name="email" type="email" required placeholder='username@gmail.com' />
+      <label htmlFor="password">Password</label>
+      <input id="password" name="password" type="password" required placeholder='Password' />
       <button formAction={login}>Log in</button>
+      <p>Or Continue With</p>
     </form>
   );
 }
